fb8e50d2bf10fbd1f886c2a7a28a088b86e2de16
Vanessa Barbosa dos Santos <vanessabsantoseng@gmail.com>
2024-09-06 18:04:11 -0300

    projeto inicial

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ varP= document.querySelector('p');
 varP.innerHTML = 'Type a number between 1 and 10';
 
 let secret_number = 5; //Math.floor((Math.random()*10)+1);
+let guess_count = 0;
 function verificarChute() {
     let guess = document.querySelector('input').value;
+    guess_count++;
 
     if (secret_number == guess) { //condição se acertar
-        varH1.innerHTML = (`Well done, you win S2!`);
+        let word_attempts = guess_count > 1 ? 'attempts' : 'attempt'; //substituto para if else
+        varH1.innerHTML = (`Well done, you win in ${guess} ${word_attempts} S2!`);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else { //condição se errar - dicas de chute
         if(secret_number < guess){
